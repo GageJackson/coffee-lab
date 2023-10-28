1b1b7fc9a98f6d3dfb6fa3cdc5f0f65693b2361a
GageJackson <wm.gagejackson@gmail.com>
2023-10-28 13:40:19 -0500

    test 4

---
+++ src/main/java/com/macguffin/coffeelab/entity/Coffee2.java
@@ -0,0 +1,28 @@
+package com.macguffin.coffeelab.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "coffee")
+@Data
+public class Coffee2 {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "country")
+    private String country;
+    @Column(name = "description")
+    private String description;
+    @Column(name = "copies")
+    private int copies;
+    @Column(name = "copies_available")
+    private int copiesAvailable;
+    @Column(name = "category")
+    private String category;
+    @Column(name = "img")
+    private String img;
+}
