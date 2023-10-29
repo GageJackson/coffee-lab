26f07d73f365d11259ccefb6494a9f13eb45a71d
GageJackson <wm.gagejackson@gmail.com>
2023-10-29 12:16:32 -0500

    should be all set to move forward

--- src/main/resources/react-frontend/src/App.tsx
+++ src/main/resources/react-frontend/src/App.tsx
@@ -3,12 +3,14 @@ import './App.css';
 import {Navbar} from "./layouts/NavbarAndFooter/Navbar";
 import {Footer} from "./layouts/NavbarAndFooter/Footer";
 import {HomePage} from "./layouts/HomePage/HomePage";
+import {SearchCoffeesPage} from "./layouts/SearchCoffeesPage/SearchCoffeesPage";
 
 export const App = () => {
   return (
       <>
         <Navbar/>
-        <HomePage/>
+        {/*<HomePage/>*/}
+          <SearchCoffeesPage/>
         <Footer/>
       </>
   );
